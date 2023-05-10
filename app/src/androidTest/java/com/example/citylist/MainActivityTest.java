1ac5ba01870fd1054bfb1c5ba6bbc0432d52afcf
Elin-powS <sopnil493@gmail>
2023-05-10 10:31:35 +0600

    f

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -110,7 +110,10 @@ public class MainActivityTest {
         Espresso.pressBack();
         onView(withId(R.id.button_confirm)).perform(click());
 
-
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Faridpur")); //Type a city name
+        Espresso.pressBack();
+        onView(withId(R.id.button_confirm)).perform(click());
 
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
         onView(withId(R.id.showactivity)).check(matches(isDisplayed()));
